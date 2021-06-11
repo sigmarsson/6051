681b83eee259d4a5e708025c7b2874ba755d860a
Tommy Gombar <Tommy@localhost>
2021-06-11 14:09:40 +0200

    Task: https://github.com/unoplatform/uno/issues/6219

---
+++ Temp.Shared/ConsolePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Prism.Regions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Weather.History.Pages
+{
+    public sealed partial class ConsolePage : Page
+    {
+        public ConsolePage()
+        {
+            InitializeComponent();
+
+            
+        }
+    }
+}
